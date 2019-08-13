2af151bec9a2aa1cafb7b6047e35ef28ea7b5dbb
Training_B4C.05.15 <Training_B4C.05.15@BDC7-D-1SS2N62.dir.svc.accenture.com>
2019-08-13 17:02:28 +0530

    Commit1

---
+++ JavaDemo/src/pack1/Calculator.java
@@ -0,0 +1,14 @@
+package pack1;
+
+public class Calculator {
+	int emp1=0;//instance variable or class variable
+	public static void main(String[] args) {
+		int mark1=10;//local variable
+		int mark2=12;
+		int tmark=mark1+mark2;
+		System.out.println("The Total Mark is: "+tmark);
+		
+	}
+
+
+}
